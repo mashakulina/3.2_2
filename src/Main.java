95e5b50e607b5df7c3c6f9bcbb094cd2f0f0653b
mashakulina <masha_kulina@mail.ru>
2022-04-28 15:05:23 +0300

    Расчет бонусов

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bill = 100; //изначальный баланс
+        int refill = 1100; //пополнение счета на сумму
+
+//        int bonus;
+//        if (refill>1000) {
+//            bonus = refill/100;
+//        } else {
+//            bonus = 0;
+//        }
+
+        int bonus = (refill>1000) ? (refill/100) : 0;
+
+        System.out.println("Начисленно бонусов: " + bonus);
+        System.out.println("Баланс: " + (refill+bill+bonus));
+
+    }
+}
